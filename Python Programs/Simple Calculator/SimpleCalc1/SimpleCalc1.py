19818b5a90da868d678034a1e8e175eaf3f1b7fe
Arul Murugan S <110210302+Arul-Murugan-S@users.noreply.github.com>
2022-08-19 17:13:56 +0530

    Add files via upload

---
+++ Python Programs/Simple Calculator/SimpleCalc1/SimpleCalc1.py
@@ -0,0 +1,18 @@
+class Calculator():
+    def init(self):
+        pass
+    
+    def addition(self,a,b):
+        add=a+b
+        return add
+    def substraction(self,a,b):
+        sub=a-b
+        return sub
+    def multiplication(self,a,b):
+        mul=a*b
+        return mul
+    def division(self,a,b):
+        div=a/b
+        return div
+
+
